50e1faf6a0cf78e2260c032f3ee46cd86e262d6d
Alex Popkov <poalrom@yandex-team.ru>
2023-04-02 17:00:31 +0300

    feat: изменяет title и добавляет react-bootstrap

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Скажи "Hello" <a href="https://nextjs.org">Next.js!</a>
+          Скажи "Hello World"!
         </h1>
 
         <p className={styles.description}>
